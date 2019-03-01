e1eac59fe838e3625c127596c2dd2af26ccac092
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 22:28:40 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,12 @@ def first_challenge
   }
 
   #your code here
-
+  contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete("strawberry")
+    end
+  end
+    
 
   #remember to return your newly altered contacts hash!
   contacts
