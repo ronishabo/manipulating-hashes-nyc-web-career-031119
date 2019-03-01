eb3790706cd0c01a502eb558680b3eb991ef6e76
Roni Shabo <roni.shabo@gmail.com>
2019-03-01 22:20:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,9 @@ def first_challenge
   }
 
   #your code here
-  
+  contacts["Freddy Mercury"].each do |attribute, value|
+    
+    
 
   #remember to return your newly altered contacts hash!
   contacts
